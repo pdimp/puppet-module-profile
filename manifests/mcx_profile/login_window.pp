aa7c9dc9240c936f24cb89e4427436a74419f422
groob <vrancean+github@gmail.com>
2014-07-14 02:27:48 -0400

    clean up and add documentation

--- manifests/mcx_profile/login_window.pp
+++ manifests/mcx_profile/login_window.pp
@@ -1,4 +1,9 @@
-class profile::mcx_profile::login_window ($ensure = 'present',$loginwindowtext="NONE") {
+# This class applies a profile to manage the login window text
+# on Mac OS X Workstations
+class profile::mcx_profile::login_window (
+  $ensure = 'present',
+  $loginwindowtext='NONE'
+) {
   mac_profiles_handler::manage { 'edu.whitby.profile.loginwindowtext':
   ensure      => $ensure,
   file_source => template('profile/mcx_profile/edu.whitby.profile.loginwindowtext.erb'),
--- manifests/mcx_profile/safari_homepage.pp
+++ manifests/mcx_profile/safari_homepage.pp
@@ -1,7 +1,9 @@
+# This class applies a profile to manage the Safari homepage
+# on Mac OS X Workstations
 class profile::mcx_profile::safari_homepage (
   $ensure = 'present',
   $homepage=""
-  ) {
+) {
   mac_profiles_handler::manage { 'edu.whitby.profile.safari_homepage':
   ensure      => $ensure,
   file_source => template('profile/mcx_profile/edu.whitby.profile.safari_homepage.erb'),
