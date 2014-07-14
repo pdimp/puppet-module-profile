6c117f67edffb83d59386ebd273d64ea31d40170
groob <vrancean+github@gmail.com>
2014-07-14 12:35:33 -0400

    manage ipv6 settings

---
+++ templates/network_settings/ipv6off.pp
@@ -0,0 +1,9 @@
+# Turns IPv6 WiFi settings off.
+class profile::network_settings::ipv6off (
+) {
+  exec {"ipv6off_wifi":
+    command => "/usr/sbin/networksetup -setv6off Wi-Fi",
+    unless  => "/usr/sbin/networksetup -getinfo Wi-Fi | grep 'IPv6: Off'"
+  }
+}
+
