b8569654147e94cdcaca12f92ea524c14440110f
groob <vrancean+github@gmail.com>
2014-07-13 23:02:22 -0400

    added login window profile

---
+++ manifests/mcx_profile/login_window.pp
@@ -0,0 +1,7 @@
+class profile::mcx_profile::login_window ($ensure = 'present') {
+  mac_profiles_handler::manage { 'edu.whitby.profile.loginwindowtext':
+  ensure      => $ensure,
+  file_source => template('profile/mcx_profile/edu.whitby.profile.loginwindowtext.erb'),
+  type        => 'template',
+  }
+}
