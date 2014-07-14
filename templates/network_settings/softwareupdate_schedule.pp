4c28d9724982adbe876789f0a8332d44345b3c1d
groob <vrancean+github@gmail.com>
2014-07-14 12:45:35 -0400

    softwareupdate schedule

---
+++ templates/network_settings/softwareupdate_schedule.pp
@@ -0,0 +1,11 @@
+# Turns IPv6 WiFi settings on/off.
+# Off by default.
+class profile::network_settings::softwareupdate_schedule (
+  $schedule = 'off'
+) {
+  exec {"softwareupdateoff":
+    command => "/usr/sbin/softwareupdate --schedule $schedule",
+    unless  => "/usr/sbin/softwareupdate --schedule | grep \"is $schedule\""
+  }
+}
+
