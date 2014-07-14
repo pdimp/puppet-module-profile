6e8a336d90add8efacec3550cc8d2ff8d1fdc857
groob <vrancean+github@gmail.com>
2014-07-14 01:16:13 -0400

    moved loginwindowtext message from profile to hiera

--- manifests/mcx_profile/login_window.pp
+++ manifests/mcx_profile/login_window.pp
@@ -1,4 +1,4 @@
-class profile::mcx_profile::login_window ($ensure = 'present') {
+class profile::mcx_profile::login_window ($ensure = 'present',$loginwindowtext="") {
   mac_profiles_handler::manage { 'edu.whitby.profile.loginwindowtext':
   ensure      => $ensure,
   file_source => template('profile/mcx_profile/edu.whitby.profile.loginwindowtext.erb'),
