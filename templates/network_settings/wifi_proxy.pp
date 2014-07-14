85d587a70f073c7bcfb9786749171dde53d2de24
groob <vrancean+github@gmail.com>
2014-07-14 12:52:32 -0400

    added a profile to manage the proxy settings

---
+++ templates/network_settings/wifi_proxy.pp
@@ -0,0 +1,17 @@
+# Configures the proxy settings to point to a pac file.
+#
+class profile::network_settings::wifi_proxy (
+  $autoproxyurl = 'http://localhost/proxy.pac'
+) {
+  exec {'autoproxyurl':
+    command => "/usr/sbin/networksetup -setautoproxyurl wi-fi $autoproxyurl",
+  }
+  exec {'autoproxy':
+    command => '/usr/sbin/networksetup -setproxyautodiscovery wi-fi on',
+    unless  => '/usr/sbin/networksetup -getproxyautodiscovery wi-fi | grep "Discovery: On"',
+  }
+  exec {'autoproxystate':
+    command => '/usr/sbin/networksetup -setautoproxystate wi-fi on',
+  }
+}
+
