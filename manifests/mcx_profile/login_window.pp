c616ae4d28697837681953783fa489c4b4aa1233
groob <vrancean+github@gmail.com>
2014-07-14 01:58:45 -0400

    changed loginwindow text to erb conditional

--- manifests/mcx_profile/login_window.pp
+++ manifests/mcx_profile/login_window.pp
@@ -1,4 +1,4 @@
-class profile::mcx_profile::login_window ($ensure = 'present',$loginwindowtext="") {
+class profile::mcx_profile::login_window ($ensure = 'present',$loginwindowtext="NONE") {
   mac_profiles_handler::manage { 'edu.whitby.profile.loginwindowtext':
   ensure      => $ensure,
   file_source => template('profile/mcx_profile/edu.whitby.profile.loginwindowtext.erb'),
