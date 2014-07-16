e76f902ce67fedfdf8c1dca04ae2c59d572c2065
groob <vrancean+github@gmail.com>
2014-07-16 12:51:46 -0400

    added wifi enrollment profile

---
+++ manifests/mcx_profile/wifi.pp
@@ -0,0 +1,10 @@
+class profile::mcx_profile::wifi (
+  $ensure = 'present'
+) {
+  mac_profiles_handler::manage { 'com.apple.mdm.od1.wifi.net.0bd012e0-ea71-0130-fcc9-002436f4cf08.alacarte':
+  ensure  => $ensure,
+  file_source => template('profile/mcx_profile/edu.whitby.profile.wifi.erb'),
+  type        => 'template',
+  }
+}
+
