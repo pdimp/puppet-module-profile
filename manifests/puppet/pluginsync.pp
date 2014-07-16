029ddd496b0086319b32bd59f03b4c70d14e45ef
groob <vrancean+github@gmail.com>
2014-07-16 18:09:52 -0400

    pluginsync with puppet apply =)

---
+++ manifests/puppet/pluginsync.pp
@@ -0,0 +1,12 @@
+# A hack to get around the fact that pluginsync doesn't work properly
+# when using 'puppet apply'
+# Will recursively sync the plugins folder to the right directory.
+class profile::puppet::pluginsync (
+  $libdir = "/var/lib/puppet/lib",
+) {
+  file {"$libdir":
+    source  => "puppet:///plugins",
+    recurse => true,
+    purge   => true,
+  }
+}
