d0028d245fde3242230bf303485c4d367b9d969a
groob <vrancean+github@gmail.com>
2014-07-16 21:54:41 -0400

    added wifi profile defaults

--- manifests/mcx_profile/wifi.pp
+++ manifests/mcx_profile/wifi.pp
@@ -1,5 +1,7 @@
 class profile::mcx_profile::wifi (
-  $ensure = 'present'
+  $ensure = 'present',
+  $ssid = 'NONE',
+  $password = 'NONE'
 ) {
   mac_profiles_handler::manage { 'com.apple.mdm.od1.wifi.net.0bd012e0-ea71-0130-fcc9-002436f4cf08.alacarte':
   ensure  => $ensure,
