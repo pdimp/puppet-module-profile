2c1c9cf6ed0b35efb0fec42e36ed5bced19ffb36
groob <vrancean+github@gmail.com>
2014-07-16 22:35:58 -0400

    renamed schedule to sched

--- manifests/mac_settings/softwareupdate_schedule.pp
+++ manifests/mac_settings/softwareupdate_schedule.pp
@@ -1,11 +1,11 @@
 # Turns IPv6 WiFi settings on/off.
 # Off by default.
 class profile::mac_settings::softwareupdate_schedule (
-  $schedule = 'off'
+  $sched = 'off'
 ) {
   exec {"softwareupdateoff":
-    command => "/usr/sbin/softwareupdate --schedule $schedule",
-    unless  => "/usr/sbin/softwareupdate --schedule | grep \"is $schedule\""
+    command => "/usr/sbin/softwareupdate --schedule $sched",
+    unless  => "/usr/sbin/softwareupdate --schedule | grep \"is $sched\""
   }
 }
 
