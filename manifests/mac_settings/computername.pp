9f8c411792c23c0b4bf6d902d274f813b8b975c4
groob <vrancean+github@gmail.com>
2014-10-20 10:00:38 -0400

    exclude root account from changing computer name

--- manifests/mac_settings/computername.pp
+++ manifests/mac_settings/computername.pp
@@ -1,15 +1,20 @@
 class profile::mac_settings::computername {
 
-  exec {"setcomputername":
-    command => "/usr/sbin/systemsetup -setcomputername $mac_current_user.$sp_serial_number",
-    unless  => "/usr/sbin/systemsetup -getcomputername | grep $mac_current_user ",
+  if $::mac_current_user == "root" {
+    notice ("root account")
   }
-  exec {"sethostname":
-    command => "/usr/sbin/scutil --set HostName $mac_current_user.$sp_serial_number",
-    unless  => "/usr/sbin/scutil --get HostName | grep $mac_current_user.$sp_serial_number",
-  }
-  exec {"setlocalhostname":
-    command => "/usr/sbin/systemsetup -setlocalsubnetname $sp_serial_number",
-    unless  => "/usr/sbin/systemsetup -getlocalsubnetname | grep $sp_serial_number",
+  else {
+    exec {"setcomputername":
+      command => "/usr/sbin/systemsetup -setcomputername $mac_current_user.$sp_serial_number",
+      unless  => "/usr/sbin/systemsetup -getcomputername | grep $mac_current_user ",
+    }
+    exec {"sethostname":
+      command => "/usr/sbin/scutil --set HostName $mac_current_user.$sp_serial_number",
+      unless  => "/usr/sbin/scutil --get HostName | grep $mac_current_user.$sp_serial_number",
+    }
+    exec {"setlocalhostname":
+      command => "/usr/sbin/systemsetup -setlocalsubnetname $sp_serial_number",
+      unless  => "/usr/sbin/systemsetup -getlocalsubnetname | grep $sp_serial_number",
+    }
   }
 }
